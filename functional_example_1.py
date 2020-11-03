9724aca7610466ff6d02d6a498b46c0728e36bfd
c.bertolotto <c.bertolotto@Reply.it>
2020-11-03 20:07:57 +0100

    Finsh to modify functional exmple

--- functional_example_1.py
+++ functional_example_1.py
@@ -17,3 +17,19 @@ my_strings = ['a', 'b', 'c', 'd', 'e']
 my_numbers = [5,4,3,2,1]
 
 print(list(zip(my_strings, sorted(my_numbers))))
+
+
+#3 Filter the scores that pass over 50%
+scores = [73, 20, 65, 19, 76, 100, 88]
+
+def is_smart_student(score):
+    return score > 50
+
+print(list(filter(is_smart_student, scores)))
+
+#4 Combine all of numbers that are in a list on this file using reduce
+
+def accumulator(acc, item):
+    return acc + item
+
+print(reduce(accumulator, (my_numbers + scores)))
